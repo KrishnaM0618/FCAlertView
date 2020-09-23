07d0125b3e1ce2f12662fcfa5b815e7b1d8a05a6
Krishnamurthy Manjini <krishnamanjini@gmail.com>
2020-09-23 10:34:55 +0530

    Update IAlertView.m

--- FCAlertView/Classes/IAlertView.m
+++ FCAlertView/Classes/IAlertView.m
@@ -1137,7 +1137,7 @@
 
 +(NSBundle *)getResourcesBundle
 {
-    NSBundle *bundle = [NSBundle bundleWithURL:[[NSBundle bundleForClass:[self class]] URLForResource:@"IAlertView" withExtension:@"bundle"]];
+    NSBundle *bundle = [NSBundle bundleWithURL:[[NSBundle bundleForClass:[self class]] URLForResource:@"FCAlertView" withExtension:@"bundle"]];
     return bundle;
 }
 
